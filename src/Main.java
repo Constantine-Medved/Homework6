57a32c34e481adeb88f8ae3345cebbc3d694433e
Constantine-Medved <medved.k@outlook.com>
2023-03-10 23:09:41 +0300

    Final Commit Empty lines were deleted

--- src/Main.java
+++ src/Main.java
@@ -11,35 +11,30 @@ public class Main {
         task9();
         task10();
     }
-
     public static void task1() {
         System.out.println("Задача 1.");
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
     }
-
     public static void task2() {
         System.out.println("Задача 2.");
         for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
     }
-
     public static void task3() {
         System.out.println("Задача 3.");
         for (int i = 2; i <= 17; i = i + 2) {
             System.out.println(i);
         }
     }
-
     public static void task4() {
         System.out.println("Задача 4.");
         for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
     }
-
     public static void task5() {
         System.out.println("Задача 5.");
         for (int year = 1904; year <= 2096; year++) {
@@ -48,7 +43,6 @@ public class Main {
             }
         }
     }
-
     public static void task6() {
         System.out.println("Задача 6.");
         for (int i = 7; i <= 98; i = i + 7) {
@@ -56,7 +50,6 @@ public class Main {
         }
         System.out.println();
     }
-
     public static void task7() {
         System.out.println("Задача 7.");
         for (int i = 1; i <= 512; i = i * 2) {
@@ -64,7 +57,6 @@ public class Main {
         }
         System.out.println();
     }
-
     public static void task8() {
         System.out.println("Задача 8.");
         int monthSavings = 29000;
@@ -74,7 +66,6 @@ public class Main {
             System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSavings + " рублей.");
         }
     }
-
     public static void task9() {
         System.out.println("Задача 9.");
         int monthSavings = 29000;
@@ -85,7 +76,6 @@ public class Main {
             System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSavings + " рублей.");
         }
     }
-
     public static void task10() {
         System.out.println("Задача 10.");
         for (int i = 1; i <= 10; i++) {
