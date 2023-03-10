05463ede25b2ca94399063387ba79c0ffdeae7db
Constantine-Medved <medved.k@outlook.com>
2023-03-10 21:09:07 +0300

    Tasks 1 - 4 completed

--- src/Main.java
+++ src/Main.java
@@ -11,27 +11,34 @@ public class Main {
         task9();
         task10();
     }
-
     public static void task1() {
         System.out.println("Задача 1.");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
-
     public static void task2() {
         System.out.println("Задача 2.");
+        for (int i = 10; i >=1; i --) {
+            System.out.println(i);
+        }
     }
-
     public static void task3() {
         System.out.println("Задача 3.");
+        for (int i = 2; i <=17; i = i + 2) {
+            System.out.println(i);
+        }
     }
-
     public static void task4() {
         System.out.println("Задача 4.");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
     }
-
     public static void task5() {
         System.out.println("Задача 5.");
-    }
 
+    }
     public static void task6() {
         System.out.println("Задача 6.");
     }
