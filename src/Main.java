b3119fe83199cc572635b21240060e5add93614d
Constantine-Medved <medved.k@outlook.com>
2023-03-10 22:50:31 +0300

    Tasks 8 - 10 completed

--- src/Main.java
+++ src/Main.java
@@ -11,47 +11,54 @@ public class Main {
         task9();
         task10();
     }
+
     public static void task1() {
         System.out.println("Задача 1.");
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
     }
+
     public static void task2() {
         System.out.println("Задача 2.");
-        for (int i = 10; i >=1; i --) {
+        for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
     }
+
     public static void task3() {
         System.out.println("Задача 3.");
-        for (int i = 2; i <=17; i = i + 2) {
+        for (int i = 2; i <= 17; i = i + 2) {
             System.out.println(i);
         }
     }
+
     public static void task4() {
         System.out.println("Задача 4.");
         for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
     }
+
     public static void task5() {
         System.out.println("Задача 5.");
         for (int i = 1904; i <= 2096; i = i + 4) {
-            if (i % 100 == 0 && i % 400 !=0) {
+            if (i % 100 == 0 && i % 400 != 0) {
                 System.out.println(i + " год НЕ является високосным");
             } else {
                 System.out.println(i + " год является високосным");
             }
         }
     }
+
     public static void task6() {
         System.out.println("Задача 6.");
-        for (int i = 7; i <= 98; i = i +7) {
+        for (int i = 7; i <= 98; i = i + 7) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
+
     public static void task7() {
         System.out.println("Задача 7.");
         for (int i = 1; i <= 512; i = i * 2) {
@@ -59,32 +66,32 @@ public class Main {
         }
         System.out.println();
     }
+
     public static void task8() {
         System.out.println("Задача 8.");
         int monthSavings = 29000;
         int totalSavings = 0;
-        for (int i =1; i <= 12;i++) {
+        for (int i = 1; i <= 12; i++) {
             totalSavings = totalSavings + monthSavings;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSavings + " рублей.");
         }
     }
+
     public static void task9() {
         System.out.println("Задача 9.");
         int monthSavings = 29000;
         int totalSavings = 0;
-        for (int i =1; i <= 12; i++) {
+        for (int i = 1; i <= 12; i++) {
             totalSavings = totalSavings + totalSavings / 100;
             totalSavings = totalSavings + monthSavings;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSavings + " рублей.");
         }
     }
+
     public static void task10() {
         System.out.println("Задача 10.");
-        int a = 2;
-        int result;
-        for (int i = 1; i <=10; i++) {
-            result = a * i;
-            System.out.println(a + " * " + i + " = " + result);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2 * " + i + " = " + i * 2);
         }
     }
 }
